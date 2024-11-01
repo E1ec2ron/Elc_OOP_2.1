f31d1b8762b52f86c5700b039b1dd27e2e5b93ac
Владислав Восьмячкин <gr424_vovva@lab-402-10.tomtit-tomsk.ru>
2024-11-01 09:35:59 +0700

    Finished(1)

--- design/main/views.py
+++ design/main/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     user_requests = Request.objects.filter(user=request.user).order_by('-created_at')[:4]
 
-    return render(request, 'main/index.html', {'completed_requests': user_requests})
+    return render(request, 'index.html', {'completed_requests': user_requests})
